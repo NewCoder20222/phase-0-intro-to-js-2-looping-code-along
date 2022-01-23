f009eb1282ae0f9ce8252484c48abe09e33ed34a
Bartosz T <btrafidlo@yahoo.com>
2022-01-22 21:33:41 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  let x = []
+  for (let i = 0; i < names.length; i++) {
+    x.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+  }
+  return x;
+}
+
+
+
+
+function countDown(){
+  let countdown = 10;
+  while (countdown >= 0) {
+    console.log(countdown--);
+  }
+}
+countDown(10);
